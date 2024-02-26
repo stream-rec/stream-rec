6e41fd21e4dbaca30e4339c0310c2d0a8a8271d2
hua0512 <whua0512@gmail.com>
2024-02-26 14:54:53 +0100

    [format] remove unsupported mkv format and fix wmv extension

--- stream-rec/src/main/kotlin/github/hua0512/data/VideoFormat.kt
+++ stream-rec/src/main/kotlin/github/hua0512/data/VideoFormat.kt
@@ -30,9 +30,8 @@ package github.hua0512.data
 enum class VideoFormat(val extension: String) {
   mp4("mp4"),
   avi("avi"),
-  mkv("mkv"),
   mov("mov"),
-  wmv("wmv"),
+  wmv("wmv3"),
   flv("flv");
 
   companion object {
