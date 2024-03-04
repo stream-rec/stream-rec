a8bf00fe33e3930bb18ec28f3d721320fded164d
hua0512 <whua0512@gmail.com>
2024-03-04 18:21:21 +0100

    feat(streamers) : some backend refactors

--- stream-rec-backend/src/main/kotlin/github/hua0512/backend/plugins/Routing.kt
+++ stream-rec-backend/src/main/kotlin/github/hua0512/backend/plugins/Routing.kt
@@ -1,7 +1,7 @@
 package github.hua0512.backend.plugins
 
+import github.hua0512.backend.routes.statsRoute
 import github.hua0512.backend.routes.streamerRoute
-import github.hua0512.logger
 import github.hua0512.repo.stats.SummaryStatsRepo
 import github.hua0512.repo.streamer.StreamerRepo
 import io.ktor.http.*
@@ -27,17 +27,3 @@ fun Application.configureRouting(streamerRepo: StreamerRepo, statsRepo: SummaryS
   }
 }
 
-
-fun Route.statsRoute(statsRepo: SummaryStatsRepo) {
-  get("/stats") {
-    val dateStartString = call.request.queryParameters["dateStart"]
-    val dateEndString = call.request.queryParameters["dateEnd"]
-
-    val dateStartEpoch = dateStartString?.toLong() ?: 0
-    val dateEndEpoch = dateEndString?.toLong() ?: Long.MAX_VALUE
-
-    val stats = statsRepo.getSummaryStatsFromTo(dateStartEpoch, dateEndEpoch)
-    logger.info("Stats: $stats")
-    call.respond(stats)
-  }
-}
