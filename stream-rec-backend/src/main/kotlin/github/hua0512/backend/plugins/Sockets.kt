39ce9bef1032655b7d0a8d928d910d2710d4de58
hua0512 <whua0512@gmail.com>
2024-03-19 02:12:39 +0100

    feat: add a websocket endpoint to listen streamers, online, offline and download state updates

--- stream-rec-backend/src/main/kotlin/github/hua0512/backend/plugins/Sockets.kt
+++ stream-rec-backend/src/main/kotlin/github/hua0512/backend/plugins/Sockets.kt
@@ -1,12 +1,29 @@
+@file:OptIn(FlowPreview::class)
+
 package github.hua0512.backend.plugins
 
+import github.hua0512.data.event.DownloadEvent.DownloadStateUpdate
+import github.hua0512.data.event.StreamerEvent
+import github.hua0512.logger
+import github.hua0512.plugins.event.EventCenter
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 import java.time.Duration
 
-fun Application.configureSockets() {
+@OptIn(ExperimentalCoroutinesApi::class)
+fun Application.configureSockets(json: Json) {
   install(WebSockets) {
     pingPeriod = Duration.ofSeconds(15)
     timeout = Duration.ofSeconds(15)
@@ -14,15 +31,47 @@ fun Application.configureSockets() {
     masking = false
   }
   routing {
-    webSocket("/ws") { // websocketSession
-      for (frame in incoming) {
-        if (frame is Frame.Text) {
-          val text = frame.readText()
-          outgoing.send(Frame.Text("YOU SAID: $text"))
-          if (text.equals("bye", ignoreCase = true)) {
-            close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+    webSocket("/live/update") {
+      try {
+        // collect ws response...
+        // no need
+        launch {
+          for (frame in incoming) {
+            if (frame is Frame.Text) {
+              val text = frame.readText()
+              if (text.equals("bye", ignoreCase = true)) {
+                close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+              }
+            }
           }
         }
+        // collect events from event center
+        EventCenter.events.buffer().filter {
+          it is DownloadStateUpdate || it is StreamerEvent.StreamerOnline || it is StreamerEvent.StreamerOffline
+        }.flatMapConcat {
+          if (it is DownloadStateUpdate) {
+            delay(1000)
+          }
+          flowOf(it)
+        }.collect { event ->
+          when (event) {
+            is DownloadStateUpdate -> {
+              send(Frame.Text(json.encodeToString(DownloadStateUpdate.serializer(), event)))
+            }
+
+            is StreamerEvent -> {
+              send(Frame.Text(json.encodeToString(StreamerEvent.serializer(), event)))
+            }
+
+            else -> {
+              logger.error("Unknown event: $event")
+            }
+          }
+        }
+      } catch (e: ClosedReceiveChannelException) {
+        logger.debug("onClose {}", closeReason.await())
+      } catch (e: Throwable) {
+        logger.debug("onError {}", closeReason.await())
       }
     }
   }
