2a1f68ec6461dd046ca329c3c61d1e474b82a017
hua0512 <whua0512@gmail.com>
2024-08-08 09:02:37 +0200

    feat(db): hash user password with md5

--- base/src/main/kotlin/github/hua0512/dao/AppDatabase.kt
+++ base/src/main/kotlin/github/hua0512/dao/AppDatabase.kt
@@ -67,14 +67,14 @@ import github.hua0512.data.user.UserEntity
   exportSchema = true,
   autoMigrations = [
     AutoMigration(from = 1, to = 2, spec = Migrate1To2::class),
-    AutoMigration(from = 2, to = 3)
+    AutoMigration(from = 2, to = 3),
   ]
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
   companion object {
-    const val DATABASE_VERSION = 3
+    const val DATABASE_VERSION = 4
   }
 
   abstract fun getConfigDao(): AppConfigDao
