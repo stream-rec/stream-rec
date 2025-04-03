8ddf2f0cf1fb283bf58409983e67686b84300b83
hua0512 <whua0512@gmail.com>
2025-04-03 15:04:17 +0200

    refactor: remove unused FlvMemoryProvider.kt

--- flv-processing/src/test/kotlin/github/hua0512/FixFlvTest.kt
+++ flv-processing/src/test/kotlin/github/hua0512/FixFlvTest.kt
@@ -24,33 +24,8 @@
  * SOFTWARE.
  */
 
-package github.hua0512/*
- * MIT License
- *
- * Stream-rec  https://github.com/hua0512/stream-rec
- *
- * Copyright (c) 2024 hua0512 (https://github.com/hua0512)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+package github.hua0512
 
-import github.hua0512.flv.FlvMemoryProvider
 import github.hua0512.flv.FlvMetaInfoProcessor
 import github.hua0512.flv.FlvMetaInfoProvider
 import github.hua0512.flv.operators.analyze
@@ -67,33 +42,9 @@ import kotlinx.io.asSource
 import kotlinx.io.buffered
 import org.junit.Test
 import java.io.File
+import kotlin.system.measureTimeMillis
 import kotlin.time.Duration
 
-/*
- * MIT License
- *
- * Stream-rec  https://github.com/hua0512/stream-rec
- *
- * Copyright (c) 2024 hua0512 (https://github.com/hua0512)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 
 /**
  * @author hua0512
@@ -103,34 +54,39 @@ class FixFlvTest {
 
   @Test
   fun testFix() = runTest(timeout = Duration.INFINITE) {
-    val file = File("E:/test/16_02_26-福州~ 主播恋爱脑！！！.flv")
+    val file = File("D:/test/999/16_02_26-福州~ 主播恋爱脑！！！.flv")
 
     val source = file.inputStream().asSource().buffered()
 
-    val memoryProvider = FlvMemoryProvider()
-
     val metaInfoProvider = FlvMetaInfoProvider()
-    val pathProvider = { index: Int -> "E:/test/${file.nameWithoutExtension}_fix_${index}_${Clock.System.now().toEpochMilliseconds()}.flv" }
-    val limitsProvider = { 0L to 3600.0f }
+    val pathProvider = { index: Int ->
+      "D:/test/fix/${file.nameWithoutExtension}_fix_${index}_${
+        Clock.System.now().toEpochMilliseconds()
+      }.flv"
+    }
+    val limitsProvider = { 0L to 0.0f }
 
     val streamerContext = StreamerContext("test", "", platform = "test")
-    source.asFlvFlow()
-      .process(limitsProvider, streamerContext, duplicateTagFiltering = false)
-      .analyze(metaInfoProvider, streamerContext)
-      .dump(pathProvider) { index, path, createdAt, updatedAt ->
-        println("onStreamDumped: $path, $createdAt -> $updatedAt")
-        launch {
-          val status = FlvMetaInfoProcessor.process(path, metaInfoProvider[index]!!, true)
-          if (status)
-            metaInfoProvider.remove(index)
-        }
-      }
-      .onCompletion {
-        println("onCompletion...")
-        memoryProvider.clear()
-      }
-      .collect()
 
+    val time = measureTimeMillis {
+      source.asFlvFlow()
+        .process(limitsProvider, streamerContext, duplicateTagFiltering = false)
+        .analyze(metaInfoProvider, streamerContext)
+        .dump(pathProvider) { index, path, createdAt, updatedAt ->
+          println("onStreamDumped: $path, $createdAt -> $updatedAt")
+          launch {
+            val status = FlvMetaInfoProcessor.process(path, metaInfoProvider[index]!!, true)
+            if (status)
+              metaInfoProvider.remove(index)
+          }
+        }
+        .onCompletion {
+          println("onCompletion...")
+        }
+        .collect()
+    }
+
+    println("Time: $time ms")
   }
 
 }
