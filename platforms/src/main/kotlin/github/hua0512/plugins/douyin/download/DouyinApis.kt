ba43ff8424b12a87ab72f23c370a0dff69ce66c5
hua0512 <whua0512@gmail.com>
2025-09-10 17:54:06 +0200

    fix(douyin-extractor): update WebSocket domains

--- platforms/src/main/kotlin/github/hua0512/plugins/douyin/download/DouyinApis.kt
+++ platforms/src/main/kotlin/github/hua0512/plugins/douyin/download/DouyinApis.kt
@@ -3,7 +3,7 @@
  *
  * Stream-rec  https://github.com/hua0512/stream-rec
  *
- * Copyright (c) 2024 hua0512 (https://github.com/hua0512)
+ * Copyright (c) 2025 hua0512 (https://github.com/hua0512)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -48,9 +48,9 @@ class DouyinApis {
     internal const val APP_ROOM_REFLOW = "https://webcast.amemv.com/webcast/room/reflow/info/"
 
     internal val webSocketDomains = arrayOf(
-      "wss://webcast5-ws-web-lq.douyin.com",
-      "wss://webcast5-ws-web-hl.douyin.com",
-      "wss://webcast5-ws-web-lf.douyin.com"
+      "wss://webcast100-ws-web-lq.douyin.com",
+      "wss://webcast100-ws-web-hl.douyin.com",
+      "wss://webcast100-ws-web-lf.douyin.com",
     )
     internal val randomWebSocketUrl get() = "${webSocketDomains.random()}/webcast/im/push/v2/"
   }
