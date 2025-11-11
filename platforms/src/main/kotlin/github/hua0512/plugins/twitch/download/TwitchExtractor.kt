790ea5c82b1c7ef6711f7178210a7c0b6c75e609
hua <whua0512@gmail.com>
2025-11-11 10:37:22 +0100

    fix(twitch): update gql requests tokens (#434)
    * fix(twitch): update tokens
    
    * fix(twitch): update ChannelShell token

--- platforms/src/main/kotlin/github/hua0512/plugins/twitch/download/TwitchExtractor.kt
+++ platforms/src/main/kotlin/github/hua0512/plugins/twitch/download/TwitchExtractor.kt
@@ -85,18 +85,17 @@ class TwitchExtractor(http: HttpClient, json: Json, override val url: String) : 
     val queries = arrayOf(
       buildPersistedQueryRequest(
         "ChannelShell",
-        "c3ea5a669ec074a58df5c11ce3c27093fa38534c94286dc14b68a25d5adcbf55",
+        "fea4573a7bf2644f5b3f2cbbdcbee0d17312e48d2e55f080589d053aad353f11",
         buildJsonObject {
           put("login", id)
-          put("lcpVideosEnabled", false)
         }
       ),
       buildPersistedQueryRequest(
         "StreamMetadata",
-        "059c4653b788f5bdb2f5a2d2a24b0ddc3831a15079001a3d927556a96fb0517f",
+        "b57f9b910f8cd1a4659d894fe7550ccc81ec9052c01e438b290fd66a040b9b93",
         buildJsonObject {
           put("channelLogin", id)
-          put("previewImageURL", "")
+          put("includeIsDJ", true)
         }
       ),
     )
@@ -158,7 +157,7 @@ class TwitchExtractor(http: HttpClient, json: Json, override val url: String) : 
       json,
       buildPersistedQueryRequest(
         "PlaybackAccessToken",
-        "0828119ded1c13477966434e15800ff57ddacf13ba1911c129dc2200705b0712",
+        "ed230aa1e33e07eebb8928504583da78a5173989fadfb1ac94be06a04f3cdbe9",
         buildJsonObject {
           put("isLive", true)
           put("login", id)
@@ -167,6 +166,7 @@ class TwitchExtractor(http: HttpClient, json: Json, override val url: String) : 
           put("playerType", "site")
           put("isClip", false)
           put("clipID", "")
+          put("platform", "site")
         }),
       getRequestHeaders()
     )
